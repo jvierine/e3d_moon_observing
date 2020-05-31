7534cdcec56ea2ba5743d8770006a5102d32f008
Juha Vierinen <jvierine@gmail.com>
2020-05-31 00:16:46 +0000

    asd

--- horizons_planetary.py
+++ horizons_planetary.py
@@ -34,6 +34,7 @@ def get_ephemeris(obj_id="2020 DA4",
     soe=False
     eoe=False
     dates=[]
+    jdates=[]
     azs=[]
     els=[]
     sublon=[]
@@ -46,6 +47,9 @@ def get_ephemeris(obj_id="2020 DA4",
         if soe and eoe == False:
             items=l.split(",")
             dates.append(items[0])
+            jdates.append(float(items[1]))
+            
+            
             azs.append(float(items[10]))
             els.append(float(items[11]))
             sublon.append(float(items[27]))
@@ -62,160 +66,17 @@ def get_ephemeris(obj_id="2020 DA4",
     els=n.array(els)
     sublon=n.array(sublon)
     sublat=n.array(sublat)
+    jdates=n.array(jdates)
     # convert to +/- lon
     sublon[sublon>180.0]=(360-sublon[sublon>180.0])*-1.0
-    return({"r":r,"rdot":rdot,"az":azs,"el":els,"sublon":sublon,"sublat":sublat})
-
-def plot_lines(lon,lat,gidx):
-    ranges=[]
-    i0=gidx[0]
-    i1=gidx[0]
-    for i in range(len(gidx)-1):
-        if gidx[i+1]-gidx[i] == 1:
-            i1=gidx[i+1]
-        else:
-            ranges.append((i0,i1))
-            i0=gidx[i+1]
-        
-    for r in ranges:
-        print("%d-%d"%(r[0],r[1]))
-        plt.plot(lon[r[0]:r[1]],lat[r[0]:r[1]],color="green")
-    
-
-def moon_observability():
-
-    plt.figure(figsize=(10,10))
-    
-    plt.subplot(331)
-    eph=get_ephemeris(obj_id="301",
-                      start="2022-01-01",
-                      stop="2023-01-01")
-    
-    gidx=n.where(eph["el"]>30.0)[0]
-    
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-    #plt.xlabel("Sub-radar lunar longitude (deg)")
-    plt.ylabel("Sub-radar lunar latitude (deg)")
-    plt.title("2022")
-    
-    eph=get_ephemeris(obj_id="301",
-                      start="2023-01-01",
-                      stop="2024-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-
-    plt.subplot(332)
-    plt.title("2023")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)    
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-#    plt.xlabel("Sub-radar lunar longitude (deg)")
-#    plt.ylabel("Sub-radar lunar latitude (deg)")
-    
-
-    plt.subplot(333)
-    eph=get_ephemeris(obj_id="301",
-                      start="2024-01-01",
-                      stop="2025-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-    
-    plt.title("2024")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)    
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-#    plt.xlabel("Sub-radar lunar longitude (deg)")
-#    plt.ylabel("Sub-radar lunar latitude (deg)")
-    
-
-    plt.subplot(334)
-    eph=get_ephemeris(obj_id="301",
-                      start="2025-01-01",
-                      stop="2026-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-    
-    plt.title("2025")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    #   plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)
-#    plt.xlabel("Sub-radar lunar longitude (deg)")
-    plt.ylabel("Sub-radar lunar latitude (deg)")
-
-    plt.subplot(335)
-    eph=get_ephemeris(obj_id="301",
-                      start="2026-01-01",
-                      stop="2027-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-
-    plt.title("2026")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
- #   plt.xlabel("Sub-radar lunar longitude (deg)")
-  #  plt.ylabel("Sub-radar lunar latitude (deg)")
-
-    plt.subplot(336)
-    eph=get_ephemeris(obj_id="301",
-                      start="2027-01-01",
-                      stop="2028-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-
-    plt.title("2027")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-   # plt.xlabel("Sub-radar lunar longitude (deg)")
-   # plt.ylabel("Sub-radar lunar latitude (deg)")
-
-    plt.subplot(337)
-    eph=get_ephemeris(obj_id="301",
-                      start="2028-01-01",
-                      stop="2029-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-
-    plt.title("2028")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-    plt.xlabel("Sub-radar lunar longitude (deg)")
-    plt.ylabel("Sub-radar lunar latitude (deg)")
-
-    plt.subplot(338)
-    eph=get_ephemeris(obj_id="301",
-                      start="2029-01-01",
-                      stop="2030-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-
-    plt.title("2029")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-    plt.xlabel("Sub-radar lunar longitude (deg)")
- #   plt.ylabel("Sub-radar lunar latitude (deg)")
-
-    plt.subplot(339)
-    eph=get_ephemeris(obj_id="301",
-                      start="2030-01-01",
-                      stop="2031-01-01")
-    gidx=n.where(eph["el"]>30.0)[0]
-
-    plt.title("2030")
-    plt.plot(eph["sublon"],eph["sublat"],color="lightgray")
-    plot_lines(eph["sublon"],eph["sublat"],gidx)
-#    plt.plot(eph["sublon"][gidx],eph["sublat"][gidx],".",color="green")
-    plt.xlabel("Sub-radar lunar longitude (deg)")
-#    plt.ylabel("Sub-radar lunar latitude (deg)")
+    return({"r":r,"rdot":rdot,"az":azs,"el":els,"sublon":sublon,"sublat":sublat,"jdates":jdates,"dates":dates})
 
     
-    plt.tight_layout()
-    plt.show()
+
 
 if __name__ == "__main__":
+    pass
 #    eph=check_detectability(obj_id="301",debug=True)
-    moon_observability()
+#    moon_observability()
 
     
-    
-
-
-
